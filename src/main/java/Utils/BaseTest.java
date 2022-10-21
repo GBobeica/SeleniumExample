0bbfc1c410324264110ef286b5b70ee80f002a92
unknown <beicaa@gmail.com>
2022-10-21 20:57:16 +0300

    modified POM

--- src/main/java/Utils/BaseTest.java
+++ src/main/java/Utils/BaseTest.java
@@ -13,6 +13,11 @@ import java.util.Date;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
@@ -31,14 +36,27 @@ public class BaseTest {
 	@BeforeClass
 	public void openBrowser(String browser) {
 		
+		
+		ChromeOptions option = new ChromeOptions();
+		option.addArguments("--headless");
+		option.addArguments("start-maximized");
+		
+		FirefoxBinary fierfoxBinary = new FirefoxBinary();
+		fierfoxBinary.addCommandLineOptions("--headless");
+		FirefoxOptions foptions = new FirefoxOptions();
+		foptions.setBinary(fierfoxBinary);
+		
+		
+		
 		if(browser.equalsIgnoreCase("chrome")) {
 			
-		   driver = WebDriverManager.chromedriver().create();
+		   //driver = WebDriverManager.chromedriver().create();
+			driver = new  ChromeDriver(option);
 		   
 		}else if(browser.equalsIgnoreCase("firefox")){
 			
-		   driver = WebDriverManager.firefoxdriver().create();
-
+		   // driver = WebDriverManager.firefoxdriver().create();
+             driver = new FirefoxDriver(foptions); 
 		driver.manage().window().maximize();
 		// driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 			driver.get("https://keybooks.ro");
